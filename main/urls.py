3540847d63bc26ebc3ea07705de398456a8a7bd6
sheexiong <chen97xiong@yahoo.com>
2020-02-19 14:27:54 +0800

    Reset the django-project to default state.

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import watches
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('contactus', views.contactus, name='contactus'),
-    path('watches' , views.watches, name='watches')
-]
--- timestory/asgi.py
+++ timestory/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for timestory project.
+ASGI config for TimeStory project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'timestory.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TimeStory.settings')
 
 application = get_asgi_application()
