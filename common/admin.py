4539713e5746ae4de05302c4e42ac98203f38182
sheexiong <chen97xiong@yahoo.com>
2020-05-27 00:36:30 +0800

    update admin page wif image preview and filter and search function.

--- common/admin.py
+++ common/admin.py
@@ -1,32 +1,76 @@
 from django.contrib import admin
-from common.models import Product, ProductImages
+from common.models import Product, ProductImage
 from django.contrib.auth.models import Group
+from django.utils.safestring import mark_safe
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.translation import ugettext as _
 
 # ModelAdmin Class
 
 
 class ImageAdmin(admin.TabularInline):
-    model = ProductImages
-    extra = 1
+    model = ProductImage
+    extra = 3
+    max_num = 3
+    min_num = 3
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'image':
+            # remove request to avoid "__init__() got an unexpected keyword argument 'request'" error
+            kwargs.pop("request", None)
+            kwargs['widget'] = AdminImageWidget
+            return db_field.formfield(**kwargs)
+        return super(ImageAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+
+class AdminImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        output = []
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            file_name = str(value)
+            output.append(u' <a href="%s" target="_blank"><img src="%s" alt="%s" height="100px" /></a>' %
+                          (image_url, image_url, file_name))
+        output.append(super(AdminFileWidget, self).render(
+            name, value, attrs, renderer))
+        return mark_safe(u''.join(output))
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'availability', 'updated_at')
+    list_display = ('image_display', 'Name', 'condition', 'updated_at', )
+    search_fields = ('brand', 'model', 'model_number', 'condition')
+    list_filter = ('brand', 'condition')
     inlines = [ImageAdmin]
+
+    # Show thumbnail in changeview
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'main_image':
+            # remove request to avoid "__init__() got an unexpected keyword argument 'request'" error
+            kwargs.pop("request", None)
+            kwargs['widget'] = AdminImageWidget
+            return db_field.formfield(**kwargs)
+        return super(ProductAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
     class Meta:
         model = Product
 
 
-@admin.register(ProductImages)
+@admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
+    # Show thumbnail in changeview
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'image':
+            # remove request to avoid "__init__() got an unexpected keyword argument 'request'" error
+            kwargs.pop("request", None)
+            kwargs['widget'] = AdminImageWidget
+            return db_field.formfield(**kwargs)
+        return super(ProductImageAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
     def get_model_perms(self, request):
         """
         Return empty perms dict thus hiding the model from admin index.
         """
         return {}
-    pass
 
 
 # Register your models here.
--- common/models.py
+++ common/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
 
 # Create your models here.
 
 BRAND_CHOICES = (
-    ('Rolex','Rolex'),
+    ('Rolex', 'Rolex'),
     ('Audemars Piguet', 'Audemars Piguet'),
-    ('Patek Philippe','Patek Philippe'),
-    ('Richard Mille','Richard Mille'),
-    ('Cartier','Cartier'),
+    ('Patek Philippe', 'Patek Philippe'),
+    ('Richard Mille', 'Richard Mille'),
+    ('Cartier', 'Cartier'),
 )
 
 CONDITION_CHOICES = (
@@ -24,7 +26,8 @@ class Product(models.Model):
     model_number = models.CharField(max_length=100, default="")
     availability = models.BooleanField(default=True)
     price = models.IntegerField(default=0)
-    condition = models.CharField(max_length=20, default="", choices=CONDITION_CHOICES)
+    condition = models.CharField(
+        max_length=20, default="", choices=CONDITION_CHOICES)
     description = models.TextField(default="")
     dial_desp = models.CharField(max_length=100, default="", blank=True)
     bezel = models.TextField(default="", blank=True)
@@ -35,7 +38,7 @@ class Product(models.Model):
     strap = models.TextField(default="", blank=True)
     buckle = models.TextField(default="", blank=True)
     crystal = models.TextField(default="", blank=True)
-    mainImage = models.ImageField(upload_to='images/')
+    main_image = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -45,11 +48,16 @@ class Product(models.Model):
     def __str__(self):
         return str(self.brand + " " + self.model)
 
+    def image_display(self):
+        return mark_safe('<img src="%s" style="height:100px;" />' % self.main_image.url)
+    image_display.allow_tags = True
+    image_display.short_description = 'Image'
 
-class ProductImages(models.Model):
+
+class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, default=None, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='images/')
 
     def __str__(self):
-        return str(self.product.Name)
+        return str(self.product.brand + " " + self.product.model)
