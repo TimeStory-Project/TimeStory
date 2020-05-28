300de68222e61ce8bbbdee2e41eac70031246f88
WillySeahh <willy.seah@hotmail.com>
2020-05-28 19:31:38 +0800

    added patek, rolex and richard mill new, used and sold .html

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,14 @@ urlpatterns = [
     path('<int:pk>/product', views.product, name='product'),
 
     path('rolexnew' , views.rolexnew, name='rolexnew'),
-    path('rolexused', views.rolexused, name='rolexused')
+    path('rolexused', views.rolexused, name='rolexused'),
+    path('rolexsold', views.rolexsold, name='rolexsold'),
+
+    path('richardmillenew' , views.richardmillenew, name='richardmillenew'),
+    path('richardmilleused', views.richardmilleused, name='richardmilleused'),
+    path('richardmillesold', views.richardmillesold, name='richardmillesold'),
+
+    path('patekphilippenew' , views.patekphilippenew, name='patekphilippenew'),
+    path('patekphilippeused', views.patekphilippeused, name='patekphilippeused'),
+    path('patekphilippesold', views.patekphilippesold, name='patekphilippesold')
 ]
--- main/views.py
+++ main/views.py
@@ -34,7 +34,44 @@ def rolexnew(request):
  
     return render(request, 'main/rolexnew.html' , {'watches':watches})
 
-
 def rolexused(request):
     watches = Product.objects.filter(brand="Rolex").filter(Q(condition="Used but good") | Q(condition="Used but worn"))
     return render(request, 'main/rolexused.html' , {'watches':watches})
+
+def rolexsold(request):
+    watches = Product.objects.filter(brand="Rolex", condition="Sold")
+    return render(request, 'main/rolexnew.html' , {'watches':watches})
+
+
+
+def richardmillenew(request):
+    watches = Product.objects.filter(brand="Richard Mille", condition="Brand new")
+ 
+    return render(request, 'main/richardmillenew.html' , {'watches':watches})
+
+def richardmilleused(request):
+    watches = Product.objects.filter(brand="Richard Mille").filter(Q(condition="Used but good") | Q(condition="Used but worn"))
+    return render(request, 'main/richardmilleused.html' , {'watches':watches})
+
+def richardmillesold(request):
+    watches = Product.objects.filter(brand="Richard Mille", condition="Sold")
+    return render(request, 'main/richardmillenew.html' , {'watches':watches})
+
+
+
+
+
+def patekphilippenew(request):
+    watches = Product.objects.filter(brand="Patek Philippe", condition="Brand new")
+ 
+    return render(request, 'main/patekphilippenew.html' , {'watches':watches})
+
+def patekphilippeused(request):
+    watches = Product.objects.filter(brand="Patek Philippe").filter(Q(condition="Used but good") | Q(condition="Used but worn"))
+    return render(request, 'main/patekphilippeused.html' , {'watches':watches})
+
+def patekphilippesold(request):
+    watches = Product.objects.filter(brand="Patek Philippe", condition="Sold")
+    return render(request, 'main/patekphilippenew.html' , {'watches':watches})
+
+
